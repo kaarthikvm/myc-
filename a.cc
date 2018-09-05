46f52bdf5d426fafb222f10861dc34525b2dda18
kaarthik.vm@gmail.com <kaarthik.vm@gmail.com>
2018-09-05 08:16:22 -0700

    struct modified

--- a.cc
+++ a.cc
@@ -5,25 +5,29 @@ using namespace std;
 typedef struct a{
   int a;
   int b;
-}A;
+} dummy;
 
 int main(){
 
 const char* a="";
 cout<<a;
-//char *b = "sdfsdfsdf";
 
-//a=b;
+cout<< sizeof(dummy)<<endl;
+dummy d1;
+dummy *d2;
+d2=&d1;
+cout<< d2<<endl;
+d2++;
+cout<< d2<<endl;
 
-//cout<<a;
+dummy obj1;
+obj1.a=100;
+obj1.b=200;
 
-cout<< sizeof(struct a);
-struct a b;
-struct a *A;
-A=&b;
-cout<< A<<endl;
-A++;
-cout<< A;
-
+dummy obj2;
+obj2=obj1;
+cout<<obj2.a<<endl;
+cout<<obj2.b<<endl;
 return 0;
+
 }
